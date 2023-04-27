7561dcb1a9ba0d99c1a8b4ce9e3db4d6b314854a
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-04-27 13:11:58 +0530

    Initial Commit

---
+++ JurassicParkApex/JurassicParkApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  JurassicParkApexApp.swift
+//  JurassicParkApex
+//
+//  Created by Neeraj Gupta on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct JurassicParkApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
